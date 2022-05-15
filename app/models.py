a3b6de4ca23407e29dd0ed522cd0ae049486ccf3
FeverCode <giddylancs.gl@gmail.com>
2022-05-15 18:13:01 +0300

    Updated models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from app import db, login_manager
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 @login_manager.user_loader
