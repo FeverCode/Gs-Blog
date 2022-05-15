5937e3e4320f7a2b4752bfcbbe19ed2a4844fceb
FeverCode <giddylancs.gl@gmail.com>
2022-05-15 11:46:52 +0300

    Added navbar

--- app/emails/views.py
+++ app/emails/views.py
@@ -1,3 +1,39 @@
-from flask import Blueprint
-email= Blueprint('email' ,__name__)
-from . import views,forms
+from . import auth
+from flask import render_template, url_for, flash, redirect, request, abort
+from flask_login import login_user, current_user, logout_user, login_required
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from ..import db
+
+
+@auth.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,email=form.email.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', r_form=form)
+
+
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user, form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+    return render_template('auth/login.html', loginform=form)
+
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
